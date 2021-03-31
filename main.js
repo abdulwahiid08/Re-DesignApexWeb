9358a6b4bd93ecf3cc9972f443e266d80137478f
AbdulWahiid8 <abdulwahid292000@gmail.com>
2021-03-31 14:04:58 +0700

    initial commit

---
+++ main.js
@@ -0,0 +1,5 @@
+const tangkap = document.getElementById('navbarNav');
+    tangkap.addEventListener('mouseenter', function(){
+        const tangkap2 = tangkap.getElementsByTagName('ul li');
+
+    });
